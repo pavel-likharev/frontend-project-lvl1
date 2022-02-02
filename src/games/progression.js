2d18204e18c64fa9fa8656133f4384efb44f1904
pavel-likharev <pavel7321@mail.ru>
2022-02-02 12:16:39 +0700

    refactoring function progression in pure function

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,28 +2,30 @@ import { genereateRandomNumber } from '../utilits.js';
 import createSomeGame from '../index.js';
 
 const gameRule = 'What number is missing in the progression?';
-const progressionLength = 9;
 const minProsressionIndex = 1;
 const maxProgressionIndex = 5;
 
-const createLineNumbers = () => {
-  const startNumber = genereateRandomNumber();
-  const progressionIndex = genereateRandomNumber(minProsressionIndex, maxProgressionIndex);
+const createProgressionFromNumber = (startNumber, progressionIndex) => {
+  const progressionLength = 9;
+  const progressionOfNumbers = [startNumber];
+
   let currentNumber = startNumber;
-  const lineNumbers = [startNumber];
 
   for (let i = 0; i < progressionLength; i += 1) {
     currentNumber += progressionIndex;
-    lineNumbers.push(currentNumber);
+    progressionOfNumbers.push(currentNumber);
   }
 
-  return lineNumbers;
+  return progressionOfNumbers;
 };
 
 const createGameConsitions = () => {
   const separator = ' ';
-  const lineNumbers = createLineNumbers();
-  const hiddenNumberIndex = genereateRandomNumber(0, progressionLength - 1);
+  const startNumber = genereateRandomNumber();
+  const progressionIndex = genereateRandomNumber(minProsressionIndex, maxProgressionIndex);
+
+  const lineNumbers = createProgressionFromNumber(startNumber, progressionIndex);
+  const hiddenNumberIndex = genereateRandomNumber(0, lineNumbers.length - 1);
   const hiddenNumber = lineNumbers[hiddenNumberIndex];
   lineNumbers[hiddenNumberIndex] = '..';
 
